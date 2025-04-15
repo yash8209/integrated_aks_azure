9c40449a6d7042fb6378416e3bf3b8dc1f5d28fe
yash pahalwani <65153900+yash8209@users.noreply.github.com>
2025-04-15 21:08:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,5 +46,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id  # ✅ correct
+  depends_on           = [azurerm_kubernetes_cluster.aks]
 }
+
