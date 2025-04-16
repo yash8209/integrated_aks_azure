8fe888086ae712cfe40b9e4d2c869c4c7d14a6e4
yash pahalwani <65153900+yash8209@users.noreply.github.com>
2025-04-16 20:27:21 +0530

    Update Program.cs

--- api_azure/Program.cs
+++ api_azure/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://0.0.0.0:80");
 
 var app = builder.Build();
 
